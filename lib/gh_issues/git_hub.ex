0209383e51eaf5b8c959bb9f063812e43ddd75b3
Ian Fosbery <ian.fosbery@reaktor.com>
2016-10-21 22:00:58 -0400

    Add docs and tests for docs

--- lib/gh_issues/git_hub.ex
+++ lib/gh_issues/git_hub.ex
@@ -1,8 +1,12 @@
 defmodule GhIssues.GitHub do
+
+  require Logger
+
   @github_url Application.get_env(:gh_issues, :github_url)
   @user_agent [{"User-agent", "Elixir ian@codeguy.io"}]
 
   def fetch(user, project) do
+    Logger.info "Fetching issues for #{user}'s project: #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,10 +17,13 @@ defmodule GhIssues.GitHub do
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({_, %{status_code: _, body: body}}) do
+  def handle_response({_, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     { :error, Poison.Parser.parse!(body) }
   end
 end
